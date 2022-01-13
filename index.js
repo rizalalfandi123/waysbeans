4bd47cce8aa01ce31fb5b5f6a3539f3ad8d0b421
rizalalfandi123 <rizalalfandi123456789@gmail.com>
2022-01-14 03:52:03 +0700

    fixing bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = require("./src/routes");
 const cors = require("cors");
 const { Server } = require("socket.io");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
---
+++ seeders/20220113203251-user.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          id: 2,
+          email: "admin@gmail.com",
+          password:
+            "$2b$10$V9P6as1DyBEyZ6XYaEHq9.mXo8AfQK0CR6gjHOuig.hDueGe2kSM.",
+          role: "admin",
+        },
+      ],
+      {}
+    );
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
